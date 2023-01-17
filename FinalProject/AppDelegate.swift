82825f830ae8a7c31a78da9360dd7291df46b74c
Nicolas Davenne <nico.dvne@gmail.com>
2023-01-17 09:57:18 +0100

    Ajout icons + sleeptime

--- FinalProject/AppDelegate.swift
+++ FinalProject/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         FirebaseApp.configure()
+        
+        do {
+            sleep(1);
+        }
+        
         return true
     }
 
